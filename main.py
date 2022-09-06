91986aaafebf62413c25118c93e42aeff597aae4
mtvrdik <59452463+mtvrdik@users.noreply.github.com>
2022-09-06 16:20:02 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#Susan has a weight of 60 kilograms and a height of 1.58 meters. Write Code to Calculate and Print Susan’s Body Mass Index as float. Note: Body Mass Index is a person’s weight in kilograms divided by the square of height in meters.
+weight = 60.0
+height = 1.58
+BMI = (weight/(height)**2)
+print(str(BMI)+" kg/m2 is Susan's BMI")
